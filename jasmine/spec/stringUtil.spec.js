658243a72a43adbf9e2e730531f44e4b99b37cef
David Kananda <davidkananda@gmail.com>
2017-08-01 15:45:48 +0100

    nth word delegate spec fail

--- jasmine/spec/stringUtil.spec.js
+++ jasmine/spec/stringUtil.spec.js
@@ -4,6 +4,13 @@ describe("stringUtil", function() {
         it("should return the first word of a string", function() {
             expect(stringUtil.firstWord("one two")).toBe("one");
         });
+
+        it("should delegate its logic to nthWord", function() {
+            spyOn(stringUtil, "nthWord");
+            stringUtil.firstWord("one two");
+
+            expect(stringUtil.nthWord).toHaveBeenCalled();
+        });
     });
 
     describe("nthword", function(){
